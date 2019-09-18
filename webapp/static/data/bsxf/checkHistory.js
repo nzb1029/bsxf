08890f23645cc519ae3b6a9db777e9212454bf24
pengjiakang <pengjiakang@cn.wilmar-intl.com>
2019-09-18 15:44:26 +0800

    新增字段[数量][出厂日期]

--- webapp/static/data/bsxf/checkHistory.js
+++ webapp/static/data/bsxf/checkHistory.js
@@ -1,34 +1,44 @@
 var ggridId="table_center_table";
 $(function(){
-	  jQuery("#table_center_table").jqGrid({ 
-			url:ctx+'/history/jqgrid', 
-			datatype: "json", 
-			colNames:['灭火器编号','区域', '位置','状态','巡检人','巡检时间','备注','操作'],
-			colModel:[ 
-			  {name:'equipment.eno',index:'equipment.eno', editable:false}, 
-			  {name:'equipment.area',index:'equipment.area',  editable:false}, 
-			  {name:'equipment.location',index:'equipment.location', editable:false,},
-			  {name:'runStatusDes',index:'runStatusDes',editable:false,sortable:false},
-			  {name:'checkUser.name',index:'checkUser.name',editable:false,sortable:false},
-			  {name:'checkTime',index:'checkTime', editable:false,sortable:false,formatter:"date",formatoptions:{srcformat:'Y-m-d H:i:s',newformat:'Y-m-d H:i:s'}},
-			  {name:'comments',index:'comments',editable:false},
-			  {name:'edit',index:'edit', formatter :function(value, options, rData){
-				  return "<a  style=\"cursor: pointer;\" title='详情' onclick=\"viewDetail('"+rData['id']+"')\" >查看详情</a>";
-			  },sortable:false}
-			  ], 
-			    rowNum:10,
-			    //multiselect: true,
-			    rowList:[10,20,30], 
-			    pager: '#pjqgajax',
-			    viewrecords: true, 
-			   // caption:"地块列表" ,
-			    jsonReader:{
-                   repeatitems : false
+	  jQuery("#table_center_table").jqGrid({
+          url: ctx + '/history/jqgrid',
+          datatype: "json",
+          colNames: ['灭火器编号', '区域', '位置', '数量', '状态', '巡检人', '巡检时间', '备注', '操作'],
+          colModel: [
+              {name: 'equipment.eno', index: 'equipment.eno', editable: false},
+              {name: 'equipment.area', index: 'equipment.area', editable: false},
+              {name: 'equipment.location', index: 'equipment.location', editable: false,},
+              {name: 'equipment.amount', index: 'equipment.amount', editable: false},
+              {name: 'runStatusDes', index: 'runStatusDes', editable: false, sortable: false},
+              {name: 'checkUser.name', index: 'checkUser.name', editable: false, sortable: false},
+              {
+                  name: 'checkTime',
+                  index: 'checkTime',
+                  editable: false,
+                  sortable: false,
+                  formatter: "date",
+                  formatoptions: {srcformat: 'Y-m-d H:i:s', newformat: 'Y-m-d H:i:s'}
               },
-              autowidth:true,
-              height:'auto',
-              cellEdit: true 
-			    });
+              {name: 'comments', index: 'comments', editable: false},
+              {
+                  name: 'edit', index: 'edit', formatter: function (value, options, rData) {
+                      return "<a  style=\"cursor: pointer;\" title='详情' onclick=\"viewDetail('" + rData['id'] + "')\" >查看详情</a>";
+                  }, sortable: false
+              }
+          ],
+          rowNum: 10,
+          //multiselect: true,
+          rowList: [10, 20, 30],
+          pager: '#pjqgajax',
+          viewrecords: true,
+          // caption:"地块列表" ,
+          jsonReader: {
+              repeatitems: false
+          },
+          autowidth: true,
+          height: 'auto',
+          cellEdit: true
+      });
 	 // jQuery("#"+ggridId).jqGrid('inlineNav',"#pjqgajax");
 	  
 	  
--- webapp/static/data/bsxf/equipmentZxing.js
+++ webapp/static/data/bsxf/equipmentZxing.js
@@ -1,34 +1,54 @@
 var ggridId="table_center_table";
 $(function(){
 	  //editauth="user:edit"; //用作修改删除按钮的权限控制
-	  jQuery("#table_center_table").jqGrid({ 
-			url:ctx+'/equipment/jqgrid', 
-			datatype: "json", 
-			colNames:['编号','区域', '位置','类别','有效起期','有效止期','巡检负责人','巡检频率','状态'],
-			colModel:[ 
-			  {name:'eno',index:'eno', editable:false}, 
-			  {name:'area',index:'area',  editable:false}, 
-			  {name:'location',index:'location', editable:false,},
-			  {name:'subTypeName',index:'subTypeName',editable:false,sortable:false},
-			  {name:'effDate',index:'effDate',editable:false,formatter:"date",formatoptions:{srcformat:'Y-m-d H:i:s',newformat:'Y-m-d H:i:s'}},
-			  {name:'expDate',index:'expDate', editable:false,formatter:"date",formatoptions:{srcformat:'Y-m-d H:i:s',newformat:'Y-m-d H:i:s'}},
-			  {name:'checkUser.name',index:'checkUser.name', editable:false,sortable:false},
-			  {name:'checkFreqDes',index:'checkFreqDes', editable:false,sortable:false},
-			  {name:'runStatusDes',index:'runStatusDes',editable:false}
-			  ], 
-			    rowNum:10,
-			    multiselect: true,
-			    rowList:[10,20,30], 
-			    pager: '#pjqgajax',
-			    viewrecords: true, 
-			   // caption:"地块列表" ,
-			    jsonReader:{
-                   repeatitems : false
+	  jQuery("#table_center_table").jqGrid({
+          url: ctx + '/equipment/jqgrid',
+          datatype: "json",
+          colNames: ['编号', '区域', '位置', '数量', '类别', '出厂日期', '有效起期', '有效止期', '巡检负责人', '巡检频率', '状态'],
+          colModel: [
+              {name: 'eno', index: 'eno', editable: false},
+              {name: 'area', index: 'area', editable: false},
+              {name: 'location', index: 'location', editable: false},
+              {name: 'amount', index: 'amount', editable: false},
+              {name: 'subTypeName', index: 'subTypeName', editable: false, sortable: false},
+              {
+                  name: 'productionDate',
+                  index: 'productionDate',
+                  editable: false,
+                  formatter: "date",
+                  formatoptions: {srcformat: 'Y-m-d H:i:s', newformat: 'Y-m-d H:i:s'}
               },
-              autowidth:true,
-              height:'auto',
-              cellEdit: true 
-			    });
+              {
+                  name: 'effDate',
+                  index: 'effDate',
+                  editable: false,
+                  formatter: "date",
+                  formatoptions: {srcformat: 'Y-m-d H:i:s', newformat: 'Y-m-d H:i:s'}
+              },
+              {
+                  name: 'expDate',
+                  index: 'expDate',
+                  editable: false,
+                  formatter: "date",
+                  formatoptions: {srcformat: 'Y-m-d H:i:s', newformat: 'Y-m-d H:i:s'}
+              },
+              {name: 'checkUser.name', index: 'checkUser.name', editable: false, sortable: false},
+              {name: 'checkFreqDes', index: 'checkFreqDes', editable: false, sortable: false},
+              {name: 'runStatusDes', index: 'runStatusDes', editable: false}
+          ],
+          rowNum: 10,
+          multiselect: true,
+          rowList: [10, 20, 30],
+          pager: '#pjqgajax',
+          viewrecords: true,
+			// caption:"地块列表" ,
+          jsonReader: {
+              repeatitems: false
+          },
+          autowidth: true,
+          height: 'auto',
+          cellEdit: true
+      });
 	 //查询按钮
 	 $(".form-search").submit(function(){
 	       var postData= $(this).serializeObject();
